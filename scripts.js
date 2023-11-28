f4e01aa76a8d820ac178d4a779b0a515c30814d9
DKondrasevskij05 <151103931+DKondrasevskij05@users.noreply.github.com>
2023-11-28 02:26:51 +0200

    Добуток 2 чисел

--- scripts.js
+++ scripts.js
@@ -6,4 +6,22 @@ window.onload = function() {
 function getArguments() {
     var number1 = prompt('Введіть перше число:');
     var number2 = prompt('Введіть друге число:');
+
+ if (number1 === null || number2 === null) {
+        return;
+    }
+
+    // Перевірка на пустий рядок
+    if (number1 === '' || number2 === '') {
+        alert('Будь ласка, введіть числа.');
+        getArguments();
+        return;
+    }
+
+    var mult = calcMult(parseFloat(number1), parseFloat(number2));
+    alert('Сума введених чисел: ' + mult);
+}
+
+function calcMult(num1, num2) {
+    return num1 * num2;
 }
