3e306b248028b5c0d1c2f5ec3e43b82c548728ee
DKondrasevskij05 <151103931+DKondrasevskij05@users.noreply.github.com>
2023-11-28 02:22:42 +0200

    Сума 2 чисел

--- scripts.js
+++ scripts.js
@@ -6,4 +6,22 @@ window.onload = function() {
 function getArguments() {
     var number1 = prompt('Введіть перше число:');
     var number2 = prompt('Введіть друге число:');
+
+ if (number1 === null || number2 === null) {
+        return;
+    }
+
+    // Перевірка на пустий рядок
+    if (number1 === '' || number2 === '') {
+        alert('Будь ласка, введіть числа.');
+        getArguments();
+        return;
+    }
+
+    var sum = calcSum(parseFloat(number1), parseFloat(number2));
+    alert('Сума введених чисел: ' + sum);
+}
+
+function calcSum(num1, num2) {
+    return num1 + num2;
 }
